f4c92d39a53d891111314f97d25333dcd9f730d8
RAKESHGURUGU <gurugurakesh9@gmail.com>
2025-09-14 16:49:47 +0530

    rectify some errors

--- src/about.jsx
+++ src/about.jsx
@@ -5,13 +5,13 @@ function  About() {
           <div>
             <h2>About Me</h2>
             <p className="about-text">
-              Hi, I’m Rakesh, a passionate Computer Science student currently pursuing my B.Tech in CSE at GMRIT. I’m deeply interested in Data Structures & Algorithms and Fullstack Development, where I enjoy building efficient solutions and creating impactful applications.
+              Hi, I'm Rakesh, a passionate Computer Science student currently pursuing my B.Tech in CSE at GMRIT. I'm deeply interested in Data Structures & Algorithms and Fullstack Development, where I enjoy building efficient solutions and creating impactful applications.
               I had the opportunity to do an apprenticeship at ThoughtWorks Hyderabad (MNC), which gave me real-world exposure to professional software development practices and strengthened my problem-solving and teamwork skills.
-              Apart from academics, I’m actively involved in technical communities like ACM, where I contribute to peer learning and leadership initiatives.
+              Apart from academics, I'm actively involved in technical communities like ACM, where I contribute to peer learning and leadership initiatives.
               My goal is to continuously learn, innovate, and contribute to meaningful projects that bridge creativity and technology.
             </p>
           </div>
-          <img src="/src/assets/rakeshphoto.png" alt="profile" className="profile-image" />
+          <img src="/rakeshphoto.png" alt="profile" className="profile-image" />
         </div>
       </section>
   );
